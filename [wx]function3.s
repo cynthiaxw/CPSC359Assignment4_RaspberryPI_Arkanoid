7154c44243779d36d9a6f94cddbec4172937b7d0
cynthiaxw <365683780@qq.com>
2018-03-30 18:15:58 -0600

    Add files via upload

--- [wx]function3.s
+++ [wx]function3.s
@@ -1,14 +1,12 @@
-pos mapOffset(int x, int y){
-	pos tmp;
-	tmp.x = (y - 80)/32;
-	tmp.y = (x - 600)/32;
-	return tmp;
-}
+@ Code section
+.section .text
+
+
 // Calculate the gameMap offset
 // Param:	r0 - x
-//			r1 - y	(screen coordinates)
+//		r1 - y	(screen coordinates)
 // Return:	r0 - x index
-//			r1 - y index
+//		r1 - y index
 // Note:	How return value works - gameMap[r0][r1]
 .global mapOffset
 mapOffset:	
@@ -29,7 +27,7 @@ mapOffset:
 
 // Check value pack states
 // r0 - value-pack struct pointer
-global vp_checkstate
+.global vp_checkState
 vp_checkState:
 	push	{r4-r10, lr}
 	
@@ -37,39 +35,227 @@ vp_checkState:
 	vpy		.req	r5
 	vpt		.req	r6
 	vpa		.req	r7
-	tmpAdr	.req	r8
+	vpp		.req	r8	
+	tmpAdr	.req	r9
 	
-	ldr		vpx, [r0, #4]
-	ldr		vpy, [r0, #8]
-	ldr		vpt, [r0, #12]
-	ldr		vpa, [r0, #16]
+	mov	vpp, r0
+	ldr	vpx, [r0]
+	ldr	vpy, [r0, #4]
+	ldr	vpt, [r0, #8]
+	ldr	vpa, [r0, #12]
 	
 //------------------if 0 == vp[2] //not activated-----------------//	
-	cmp		vpt, #0		//not activated
-	bne		vp_else1
+	cmp	vpt, #0		//not activated
+	bne	vp_else1
 	
-	mov		r0, vpx
-	mov		r1, vpy
-	bl		mapOffset		//get the corresponding map offset
+	mov	r0, vpx
+	mov	r1, vpy
+	bl	mapOffset		//get the corresponding map offset
 
-	ldr		tmpAdr, =gameMap
-		
+	ldr	tmpAdr, =gameMap
+	mov	r10, #22
+	mul	r0, r10 //offset = r0*22 + r1	
+	add	r0, r1
+	ldr	r10, [tmpAdr, r0, lsl #2] //gameMap[tmpx][tmpy]
 	
+	cmp	r10, #0
+	moveq	r10, #1
+	ldreq	r10, [vpp, #8]	//valuepack.state = 1 - change state to falling
 	
-	b		done		//return
+	b	done		//return
 	
 //------------------if 1 == vp[2] //falling-----------------//
 vp_else1:
-	cmp		vpt, #1		//falling
-	bne		vp_else2
+	cmp	vpt, #1		//falling
+	bne	vp_else2
 	
+	ldr	r10, =879
+	cmp	vpy, r10	//if valuepack.y > 879, lost
+	movgt	r10, #3
+	ldrgt	r10, [vpp, #8]	//valuepack.state = 3, change state to lost
+	bgt	done
+
+// caught if(vp[1] > 816 && vp[1] < 880 && vp[0] > curPaddle[0] - 64 && vp[0] < curPaddle[0] + curPaddle[2])
+	cmp	vpy, #816
+	ble	vp_else1_continue
+	cmp	vpy, #880
+	bge	vp_else1_continue
+	ldr	tmpAdr, =curPaddle
+	ldr	r10, [tmpAdr]		//curPaddle.x
+	ldr	r7, [tmpAdr, #8]	//curPadlle.length
+	sub	r10, #64
+	cmp	vpx, r10
+	ble	vp_else1_continue
+	add	r10, #64
+	add 	r10, r7
+	cmp	vpx, r10
+	bge	vp_else1_continue
 	
-	b		done		//return
+	mov	r10, #2
+	ldr	r10, [vpp, #8]	//valuepack.state = 2 - change state to caught
+	b	done
+
+vp_else1_continue:
+	//clear previous pack
+	push	{r4-r10}
+	
+	mov	r0, vpx
+	mov	r1, vpy
+	bl	mapOffset
+	mov	r4, r0		//x offset
+	mov	r5, r1		//y offset
+	ldr	tmpAdr, =gameMap
+	mov	r6, #22
+	mul	r6, r4 //offset = r6*22 + r5	
+	add	r6, r5
+	ldr	r7, [tmpAdr, r6, lsl #2] //gameMap[x_off][y_off]
+	
+	mov	r0, r4
+	mov	r1, r5
+	mov	r2, r7
+	bl	drawLeftBrick
+
+	add	r5, #1
+	mov	r6, #22
+	mul	r6, r4 //offset = r6*22 + r5	
+	add	r6, r5
+	ldr	r7, [tmpAdr, r6, lsl #2] //gameMap[x_off][y_off+1]
+
+	mov	r0, r4
+	mov	r1, r5
+	mov	r2, r7
+	bl	drawRightBrick
+	
+	ldr	r10, [vpp, #4]		//valuepack.y+=2
+	add	r10, #2
+	str	r10, [vpp, #4]
+	
+	pop	{r4-r10}
+		
+	b	done		//return
 
 //------------------if 2 == vp[2] //caught-----------------//
 vp_else2:
-	cmp		vpt, #2		//caught
-	bne		done
+	cmp	vpt, #2		//caught
+	bne	done
+
+	//clear previous value pack
+	push	{r4-r10}
+	mov	r0, vpx
+	mov	r1, vpy
+	bl	mapOffset
+	
+	mov	r4, r0		//x offset
+	mov	r5, r1		//y offset
+	ldr	tmpAdr, =gameMap
+
+	mov	r6, #22
+	mul	r6, r4 //offset = r6*22 + r5	
+	add	r6, r5
+	ldr	r7, [tmpAdr, r6, lsl #2] //gameMap[x_off][y_off]
+	mov	r0, r4
+	mov	r1, r5
+	mov	r2, r7
+	bl	drawLeftBrick	//drawLeftBrick(tmp.x,tmp.y,gameMap[tmp.x][tmp.y]);
+	
+	add	r5, #1
+	mov	r6, #22
+	mul	r6, r4 //r6 = offset = r6*22 + r5	
+	add	r6, r5
+	ldr	r7, [tmpAdr, r6, lsl #2] //gameMap[x_off][y_off+1]
+	mov	r0, r4
+	mov	r1, r5
+	mov	r2, r7	
+	bl	drawRightBrick	//drawRightBrick(tmp.x,tmp.y+1,gameMap[tmp.x][tmp.y+1]);
+
+	add	r4, #1
+	sub	r5, #1
+	mov	r6, #22
+	mul	r6, r4 //r6 = offset = r6*22 + r5	
+	add	r6, r5
+	ldr	r7, [tmpAdr, r6, lsl #2] //gameMap[x_off][y_off+1]
+	mov	r0, r4
+	mov	r1, r5
+	mov	r2, r7	
+	bl	drawLeftBrick	//drawLeftBrick(tmp.x+1,tmp.y,gameMap[tmp.x+1][tmp.y]);
+
+	add	r5, #1
+	mov	r6, #22
+	mul	r6, r4 //r6 = offset = r6*22 + r5	
+	add	r6, r5
+	ldr	r7, [tmpAdr, r6, lsl #2] //gameMap[x_off][y_off+1]
+	mov	r0, r4
+	mov	r1, r5
+	mov	r2, r7	
+	bl	drawRightBrick//drawRightBrick(tmp.x+1,tmp.y+1,gameMap[tmp.x+1][tmp.y+1]);
+
+	pop	{r4-r10}
+
+vp_ability1:
+	//if valuepack.ability == 1	
+	//extend paddle
+	cmp	vpa, #1
+	bne	vp_ability2
+
+	//curPaddle.length = 160
+	ldr	tmpAdr, =curPaddle
+	mov	r10, #160
+	str	r10, [tmpAdr, #8]
+
+	//prePaddle/length = 160
+	ldr	tmpAdr, =prePaddle
+	mov	r10, #160
+	str	r10, [tmpAdr, #8]
+
+	//stylePaddle = 2
+	ldr	tmpAdr, =stylePaddle
+	mov	r10, #2
+	str	r10, [tmpAdr]
+
+	b	vp_ability_done
+	
+vp_ability2:
+	push	{r4-r10}
+	//else if valuepack.ability == 2
+	//slow down the ball
+	cmp	vpa, #2
+	bne	vp_ability3
+
+	//plusMovement = 2
+	ldr	tmpAdr, =plusMovement
+	mov	r10, #2
+	ldr	r10, [tmpAdr]
+
+	//substract plusMovement from the abstract value of ball movement 
+	ldr	tmpAdr, =ballDirection
+	ldr	r4, [tmpAdr]		//ballDirection.x
+	ldr	r5, [tmpAdr, #4]	//ballDirection.y
+
+	cmp	r4, #0
+	subgt	r4, #2			//if ballDirection.x > 0, -=2
+	addle	r4, #2			//else, +=2
+	str	r4, [tmpAdr]
+	
+	cmp	r5, #0
+	subgt	r5, #2			//if ballDirection.y > 0, -=2
+	addle	r5, #2			//else, +=2
+	str	r5, [tmpAdr, #4]
+ 
+	pop	{r4-r10}
+	b	vp_ability_done
+
+vp_ability3:
+	//else if valuepack.ability == 3
+	//add one life
+	ldr	tmpAdr, =lives
+	ldr	r10, [tmpAdr]
+	add	r10, #1
+	str	r10, [tmpAdr]
+	bl	drawLives
+
+vp_ability_done:
+	mov	r10, #3
+	str	r10, [vpp, #8]
 
 done:					//lost
 	.unreq	vpx
@@ -78,130 +264,3 @@ done:					//lost
 	.unreq	vpa
 	.unreq	tmpAdr	
 	pop		{r4-r10, pc}
-
-
-void vp_checkState(int* vp){
-	if(0 == vp[2]){	//not activated
-		pos tmp;
-		tmp = mapOffset(vp[0], vp[1]);	//get the corresponding map offset
-
-		if(0 == gameMap[tmp.x][tmp.y]){	//brick cleared
-			vp[2] = 1;	//change value pack state to falling 
-		}
-	}
-	else if(1 == vp[2]){	//falling
-		if(vp[1] > 879){	//lost
-			vp[2] = 3;
-			return;
-		}
-		else if(vp[1] > 816 && vp[1] < 880 && vp[0] > curPaddle[0] - 64 && vp[0] < curPaddle[0] + curPaddle[2]){	//hit paddle
-			vp[2] = 2;	//caught
-			return;
-		}
-		//clear previous pack
-		pos tmp;
-		tmp = mapOffset(vp[0], vp[1]);	//get the corresponding map offset
-		drawLeftBrick(tmp.x,tmp.y,gameMap[tmp.x][tmp.y]);
-		drawRightBrick(tmp.x,tmp.y+1,gameMap[tmp.x][tmp.y+1]);
-		vp[1] += 2;
-		printf("vp_y:%d\n", vp[1]);
-		//draw current pack
-		drawVP(vp[0], vp[1], vp[3]);
-	}
-	else if(2 == vp[2]){	//caught
-		//clear previous pack
-		pos tmp;
-		tmp = mapOffset(vp[0], vp[1]);	//get the corresponding map offset
-		drawLeftBrick(tmp.x,tmp.y,gameMap[tmp.x][tmp.y]);
-		drawRightBrick(tmp.x,tmp.y+1,gameMap[tmp.x][tmp.y+1]);
-		drawLeftBrick(tmp.x+1,tmp.y,gameMap[tmp.x+1][tmp.y]);
-		drawRightBrick(tmp.x+1,tmp.y+1,gameMap[tmp.x+1][tmp.y+1]);
-
-		if(1 == vp[3]){	//extend paddle
-			curPaddle[2] = 160;
-			prePaddle[2] = 160;
-			stylePaddle = 2;
-		}
-		else if(2 == vp[3]){	//slow down ball
-			plusMovement = 2;
-			if(ballDirection[0] > 0)ballDirection[0] -= plusMovement;
-			else ballDirection[0] += plusMovement;
-			if(ballDirection[1] > 0)ballDirection[1] -= plusMovement;
-			else ballDirection[1] += plusMovement;
-		}
-		else if(3 == vp[3]){	//add one life
-			lives++;
-			drawLives();
-		}
-		vp[2] = 3;
-		return;
-	}
-	else if(3 == vp[2]){	//lost
-		return;
-	}	
-}
-
-
-void gameProcess(){
-	
-	if(pressed_button == 4){	//pause 	ST
-		gameChoice = 2;
-	}
-	
-	if(score == 60){	//win
-		gameChoice = 7;
-	}
-	else if(curBall[1] > 856 && curBall[1] < 910){
-		if(curBall[0] < 634 || curBall[0] > 1238){//hit left or right wall
-			ballDirection[0] *= -1;
-		}
-
-		cleanBall(curBall[0], curBall[1]);
-//Draw ball
-		curBall[0] += ballDirection[0];
-		curBall[1] += ballDirection[1];
-		drawABall(curBall[0], curBall[1]);	
-		preBall[0] = curBall[0];
-		preBall[1] = curBall[1];
-	}
-	else if(curBall[1] > 856){
-		lives --;
-		drawLives();
-		printf("lives: %d\n", lives);
-		if(lives == 0){
-			gameChoice = 5;
-		}
-		else{
-			//updateBall();
-			//cleanBall(curBall[0], curBall[1]);
-			curBall[0] = 944;
-			curBall[1] = 832;
-			preBall[0] = 944;
-			preBall[1] = 832;
-			ballDirection[0] = 5;
-			ballDirection[1] = -5;	
-			plusMovement = 0;
-					
-			cleanPaddle();
-								
-			prePaddle[0] = 888;
-			prePaddle[1] = 848;
-			prePaddle[2] = 128;
-			curPaddle[0] = 888;
-			curPaddle[1] = 848;
-			curPaddle[2] = 128;						
-			stylePaddle = 1;			
-			gameChoice = 6;			
-			drawABall(curBall[0], curBall[1]);	
-			draw_paddle(curPaddle[0], stylePaddle);
-		}
-	}
-	else{
-		vp_checkState(valuepack1);
-		vp_checkState(valuepack2);
-		vp_checkState(valuepack3);
-		updateBall();
-		movePaddle();
-		draw_paddle(curPaddle[0], stylePaddle);
-	}
-}
